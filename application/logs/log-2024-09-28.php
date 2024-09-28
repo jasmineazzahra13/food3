b52f00a5d33fbffc0f11bdfcfce4ce697433c41f
jasmineazzahra13 <zahrabrilliant1323@gmail.com>
2024-09-29 03:00:54 +0700

    Initial Commit

---
+++ application/logs/log-2024-09-28.php
@@ -0,0 +1,206 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2024-09-28 06:42:10 --> Config Class Initialized
+INFO - 2024-09-28 06:42:10 --> Hooks Class Initialized
+DEBUG - 2024-09-28 06:42:10 --> UTF-8 Support Enabled
+INFO - 2024-09-28 06:42:10 --> Utf8 Class Initialized
+INFO - 2024-09-28 06:42:10 --> URI Class Initialized
+DEBUG - 2024-09-28 06:42:10 --> No URI present. Default controller set.
+INFO - 2024-09-28 06:42:10 --> Router Class Initialized
+INFO - 2024-09-28 06:42:10 --> Output Class Initialized
+INFO - 2024-09-28 06:42:10 --> Security Class Initialized
+DEBUG - 2024-09-28 06:42:10 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-09-28 06:42:10 --> Input Class Initialized
+INFO - 2024-09-28 06:42:10 --> Language Class Initialized
+INFO - 2024-09-28 06:42:10 --> Language Class Initialized
+INFO - 2024-09-28 06:42:10 --> Config Class Initialized
+INFO - 2024-09-28 06:42:10 --> Loader Class Initialized
+DEBUG - 2024-09-28 06:42:10 --> Config file loaded: D:\food3\application\config/config.php
+INFO - 2024-09-28 06:42:10 --> Helper loaded: security_helper
+INFO - 2024-09-28 06:42:10 --> Helper loaded: form_helper
+INFO - 2024-09-28 06:42:11 --> Helper loaded: url_helper
+INFO - 2024-09-28 06:42:11 --> Helper loaded: global_helper
+INFO - 2024-09-28 06:42:11 --> Helper loaded: inflector_helper
+INFO - 2024-09-28 06:42:11 --> Helper loaded: text_helper
+INFO - 2024-09-28 06:42:11 --> Helper loaded: string_helper
+INFO - 2024-09-28 06:42:11 --> Helper loaded: language_helper
+INFO - 2024-09-28 06:42:11 --> Helper loaded: file_helper
+INFO - 2024-09-28 06:42:11 --> Language file loaded: language/english/auth_lang.php
+INFO - 2024-09-28 06:42:11 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2024-09-28 06:42:11 --> Database Driver Class Initialized
+ERROR - 2024-09-28 06:42:11 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 06:42:11 --> Unable to connect to the database
+INFO - 2024-09-28 06:42:11 --> Session: Class initialized using 'files' driver.
+INFO - 2024-09-28 06:42:11 --> Database Driver Class Initialized
+ERROR - 2024-09-28 06:42:11 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 06:42:11 --> Unable to connect to the database
+INFO - 2024-09-28 06:42:34 --> Config Class Initialized
+INFO - 2024-09-28 06:42:34 --> Hooks Class Initialized
+DEBUG - 2024-09-28 06:42:34 --> UTF-8 Support Enabled
+INFO - 2024-09-28 06:42:34 --> Utf8 Class Initialized
+INFO - 2024-09-28 06:42:34 --> URI Class Initialized
+DEBUG - 2024-09-28 06:42:34 --> No URI present. Default controller set.
+INFO - 2024-09-28 06:42:34 --> Router Class Initialized
+INFO - 2024-09-28 06:42:34 --> Output Class Initialized
+INFO - 2024-09-28 06:42:34 --> Security Class Initialized
+DEBUG - 2024-09-28 06:42:34 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-09-28 06:42:34 --> Input Class Initialized
+INFO - 2024-09-28 06:42:34 --> Language Class Initialized
+INFO - 2024-09-28 06:42:34 --> Language Class Initialized
+INFO - 2024-09-28 06:42:34 --> Config Class Initialized
+INFO - 2024-09-28 06:42:34 --> Loader Class Initialized
+DEBUG - 2024-09-28 06:42:34 --> Config file loaded: D:\food3\application\config/config.php
+INFO - 2024-09-28 06:42:34 --> Helper loaded: security_helper
+INFO - 2024-09-28 06:42:34 --> Helper loaded: form_helper
+INFO - 2024-09-28 06:42:34 --> Helper loaded: url_helper
+INFO - 2024-09-28 06:42:34 --> Helper loaded: global_helper
+INFO - 2024-09-28 06:42:34 --> Helper loaded: inflector_helper
+INFO - 2024-09-28 06:42:34 --> Helper loaded: text_helper
+INFO - 2024-09-28 06:42:34 --> Helper loaded: string_helper
+INFO - 2024-09-28 06:42:34 --> Helper loaded: language_helper
+INFO - 2024-09-28 06:42:34 --> Helper loaded: file_helper
+INFO - 2024-09-28 06:42:34 --> Language file loaded: language/english/auth_lang.php
+INFO - 2024-09-28 06:42:34 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2024-09-28 06:42:34 --> Database Driver Class Initialized
+ERROR - 2024-09-28 06:42:34 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 06:42:34 --> Unable to connect to the database
+INFO - 2024-09-28 06:42:34 --> Session: Class initialized using 'files' driver.
+INFO - 2024-09-28 06:42:34 --> Database Driver Class Initialized
+ERROR - 2024-09-28 06:42:34 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 06:42:34 --> Unable to connect to the database
+INFO - 2024-09-28 06:48:01 --> Config Class Initialized
+INFO - 2024-09-28 06:48:01 --> Hooks Class Initialized
+DEBUG - 2024-09-28 06:48:01 --> UTF-8 Support Enabled
+INFO - 2024-09-28 06:48:01 --> Utf8 Class Initialized
+INFO - 2024-09-28 06:48:01 --> URI Class Initialized
+DEBUG - 2024-09-28 06:48:01 --> No URI present. Default controller set.
+INFO - 2024-09-28 06:48:01 --> Router Class Initialized
+INFO - 2024-09-28 06:48:01 --> Output Class Initialized
+INFO - 2024-09-28 06:48:01 --> Security Class Initialized
+DEBUG - 2024-09-28 06:48:01 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-09-28 06:48:01 --> Input Class Initialized
+INFO - 2024-09-28 06:48:01 --> Language Class Initialized
+INFO - 2024-09-28 06:48:01 --> Language Class Initialized
+INFO - 2024-09-28 06:48:01 --> Config Class Initialized
+INFO - 2024-09-28 06:48:01 --> Loader Class Initialized
+DEBUG - 2024-09-28 06:48:01 --> Config file loaded: D:\food3\application\config/config.php
+INFO - 2024-09-28 06:48:01 --> Helper loaded: security_helper
+INFO - 2024-09-28 06:48:01 --> Helper loaded: form_helper
+INFO - 2024-09-28 06:48:01 --> Helper loaded: url_helper
+INFO - 2024-09-28 06:48:01 --> Helper loaded: global_helper
+INFO - 2024-09-28 06:48:01 --> Helper loaded: inflector_helper
+INFO - 2024-09-28 06:48:01 --> Helper loaded: text_helper
+INFO - 2024-09-28 06:48:01 --> Helper loaded: string_helper
+INFO - 2024-09-28 06:48:01 --> Helper loaded: language_helper
+INFO - 2024-09-28 06:48:01 --> Helper loaded: file_helper
+INFO - 2024-09-28 06:48:01 --> Language file loaded: language/english/auth_lang.php
+INFO - 2024-09-28 06:48:01 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2024-09-28 06:48:01 --> Database Driver Class Initialized
+ERROR - 2024-09-28 06:48:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 06:48:01 --> Unable to connect to the database
+INFO - 2024-09-28 06:48:01 --> Session: Class initialized using 'files' driver.
+INFO - 2024-09-28 06:48:01 --> Database Driver Class Initialized
+ERROR - 2024-09-28 06:48:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 06:48:01 --> Unable to connect to the database
+INFO - 2024-09-28 07:41:38 --> Config Class Initialized
+INFO - 2024-09-28 07:41:38 --> Hooks Class Initialized
+DEBUG - 2024-09-28 07:41:38 --> UTF-8 Support Enabled
+INFO - 2024-09-28 07:41:38 --> Utf8 Class Initialized
+INFO - 2024-09-28 07:41:38 --> URI Class Initialized
+DEBUG - 2024-09-28 07:41:38 --> No URI present. Default controller set.
+INFO - 2024-09-28 07:41:38 --> Router Class Initialized
+INFO - 2024-09-28 07:41:38 --> Output Class Initialized
+INFO - 2024-09-28 07:41:38 --> Security Class Initialized
+DEBUG - 2024-09-28 07:41:38 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-09-28 07:41:38 --> Input Class Initialized
+INFO - 2024-09-28 07:41:38 --> Language Class Initialized
+INFO - 2024-09-28 07:41:38 --> Language Class Initialized
+INFO - 2024-09-28 07:41:38 --> Config Class Initialized
+INFO - 2024-09-28 07:41:38 --> Loader Class Initialized
+DEBUG - 2024-09-28 07:41:38 --> Config file loaded: D:\food3\application\config/config.php
+INFO - 2024-09-28 07:41:38 --> Helper loaded: security_helper
+INFO - 2024-09-28 07:41:38 --> Helper loaded: form_helper
+INFO - 2024-09-28 07:41:38 --> Helper loaded: url_helper
+INFO - 2024-09-28 07:41:38 --> Helper loaded: global_helper
+INFO - 2024-09-28 07:41:38 --> Helper loaded: inflector_helper
+INFO - 2024-09-28 07:41:38 --> Helper loaded: text_helper
+INFO - 2024-09-28 07:41:38 --> Helper loaded: string_helper
+INFO - 2024-09-28 07:41:38 --> Helper loaded: language_helper
+INFO - 2024-09-28 07:41:38 --> Helper loaded: file_helper
+INFO - 2024-09-28 07:41:38 --> Language file loaded: language/english/auth_lang.php
+INFO - 2024-09-28 07:41:38 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2024-09-28 07:41:38 --> Database Driver Class Initialized
+ERROR - 2024-09-28 07:41:38 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 07:41:38 --> Unable to connect to the database
+INFO - 2024-09-28 07:41:38 --> Session: Class initialized using 'files' driver.
+INFO - 2024-09-28 07:41:38 --> Database Driver Class Initialized
+ERROR - 2024-09-28 07:41:38 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 07:41:38 --> Unable to connect to the database
+INFO - 2024-09-28 07:47:50 --> Config Class Initialized
+INFO - 2024-09-28 07:47:50 --> Hooks Class Initialized
+DEBUG - 2024-09-28 07:47:50 --> UTF-8 Support Enabled
+INFO - 2024-09-28 07:47:50 --> Utf8 Class Initialized
+INFO - 2024-09-28 07:47:50 --> URI Class Initialized
+DEBUG - 2024-09-28 07:47:50 --> No URI present. Default controller set.
+INFO - 2024-09-28 07:47:50 --> Router Class Initialized
+INFO - 2024-09-28 07:47:50 --> Output Class Initialized
+INFO - 2024-09-28 07:47:50 --> Security Class Initialized
+DEBUG - 2024-09-28 07:47:50 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-09-28 07:47:50 --> Input Class Initialized
+INFO - 2024-09-28 07:47:50 --> Language Class Initialized
+INFO - 2024-09-28 07:47:50 --> Language Class Initialized
+INFO - 2024-09-28 07:47:50 --> Config Class Initialized
+INFO - 2024-09-28 07:47:50 --> Loader Class Initialized
+DEBUG - 2024-09-28 07:47:50 --> Config file loaded: D:\food3\application\config/config.php
+INFO - 2024-09-28 07:47:50 --> Helper loaded: security_helper
+INFO - 2024-09-28 07:47:50 --> Helper loaded: form_helper
+INFO - 2024-09-28 07:47:50 --> Helper loaded: url_helper
+INFO - 2024-09-28 07:47:50 --> Helper loaded: global_helper
+INFO - 2024-09-28 07:47:50 --> Helper loaded: inflector_helper
+INFO - 2024-09-28 07:47:50 --> Helper loaded: text_helper
+INFO - 2024-09-28 07:47:50 --> Helper loaded: string_helper
+INFO - 2024-09-28 07:47:50 --> Helper loaded: language_helper
+INFO - 2024-09-28 07:47:50 --> Helper loaded: file_helper
+INFO - 2024-09-28 07:47:50 --> Language file loaded: language/english/auth_lang.php
+INFO - 2024-09-28 07:47:50 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2024-09-28 07:47:50 --> Database Driver Class Initialized
+ERROR - 2024-09-28 07:47:50 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 07:47:50 --> Unable to connect to the database
+DEBUG - 2024-09-28 07:47:50 --> Session: Initialization under CLI aborted.
+INFO - 2024-09-28 07:47:50 --> Database Driver Class Initialized
+ERROR - 2024-09-28 07:47:50 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 07:47:50 --> Unable to connect to the database
+INFO - 2024-09-28 07:48:03 --> Config Class Initialized
+INFO - 2024-09-28 07:48:03 --> Hooks Class Initialized
+DEBUG - 2024-09-28 07:48:03 --> UTF-8 Support Enabled
+INFO - 2024-09-28 07:48:03 --> Utf8 Class Initialized
+INFO - 2024-09-28 07:48:03 --> URI Class Initialized
+DEBUG - 2024-09-28 07:48:03 --> No URI present. Default controller set.
+INFO - 2024-09-28 07:48:03 --> Router Class Initialized
+INFO - 2024-09-28 07:48:03 --> Output Class Initialized
+INFO - 2024-09-28 07:48:03 --> Security Class Initialized
+DEBUG - 2024-09-28 07:48:03 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-09-28 07:48:03 --> Input Class Initialized
+INFO - 2024-09-28 07:48:03 --> Language Class Initialized
+INFO - 2024-09-28 07:48:03 --> Language Class Initialized
+INFO - 2024-09-28 07:48:03 --> Config Class Initialized
+INFO - 2024-09-28 07:48:03 --> Loader Class Initialized
+DEBUG - 2024-09-28 07:48:03 --> Config file loaded: D:\food3\application\config/config.php
+INFO - 2024-09-28 07:48:03 --> Helper loaded: security_helper
+INFO - 2024-09-28 07:48:03 --> Helper loaded: form_helper
+INFO - 2024-09-28 07:48:03 --> Helper loaded: url_helper
+INFO - 2024-09-28 07:48:03 --> Helper loaded: global_helper
+INFO - 2024-09-28 07:48:03 --> Helper loaded: inflector_helper
+INFO - 2024-09-28 07:48:03 --> Helper loaded: text_helper
+INFO - 2024-09-28 07:48:03 --> Helper loaded: string_helper
+INFO - 2024-09-28 07:48:03 --> Helper loaded: language_helper
+INFO - 2024-09-28 07:48:03 --> Helper loaded: file_helper
+INFO - 2024-09-28 07:48:03 --> Language file loaded: language/english/auth_lang.php
+INFO - 2024-09-28 07:48:03 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2024-09-28 07:48:03 --> Database Driver Class Initialized
+ERROR - 2024-09-28 07:48:03 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 07:48:03 --> Unable to connect to the database
+DEBUG - 2024-09-28 07:48:03 --> Session: Initialization under CLI aborted.
+INFO - 2024-09-28 07:48:03 --> Database Driver Class Initialized
+ERROR - 2024-09-28 07:48:03 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) D:\food3\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-09-28 07:48:03 --> Unable to connect to the database
